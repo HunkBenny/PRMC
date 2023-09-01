0c766891c4cb0d96d63bf65979a54ae71c4890dc
HunkBenny <samueljbakker@outlook.com>
2023-09-01 11:55:23 +0200

    [upd]

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 
-echo -e "#! /bin/bash \npython -m shiny run --port 60267" > ./start.sh
+echo -e "#! /bin/bash\nopen http://localhost:60267 \npython -m shiny run --port 60267" > ./start.sh
 ./start.sh
