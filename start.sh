70092ca3aba2412e09a87967910a78e363143f67
HunkBenny <samueljbakker@outlook.com>
2023-09-12 08:23:31 +0200

    [UPD] fixed start script

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-open http://localhost:60267 
-python -m shiny run --port 60267
+
+pip install -r requirements.txt
+
+echo -e "#! /bin/bash \npython -m shiny run --port 60267" > ./start.sh
+./start.sh
