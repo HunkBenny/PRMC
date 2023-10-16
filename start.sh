819782210c5fa32f125456fcd24760df52ddb556
HunkBenny <80793807+HunkBenny@users.noreply.github.com>
2023-10-16 11:18:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-#! /bin/bash 
-echo "App running on port 60267"
-echo "http://localhost:60267"
-python3 -m shiny run --port 60267
+#! /bin/bash
+
+pip install -r requirements.txt
+
+echo -e "#! /bin/bash \necho \"app running on port 60267\"\npython3 -m shiny run --port 60267" > ./start.sh
+./start.sh
