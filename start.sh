9c41653137ebbdfb3a058eef48547a63bb8ca0e9
HunkBenny <80793807+HunkBenny@users.noreply.github.com>
2023-10-16 11:16:34 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 
-echo -e "#! /bin/bash \npython -m shiny run --port 60267" > ./start.sh
+echo -e "#! /bin/bash \necho \"app running on port 60267\"\npython3 -m shiny run --port 60267" > ./start.sh
 ./start.sh
